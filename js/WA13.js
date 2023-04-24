424b742b3d448c3cd05518fd1249503788708931
Conor Fagan <84397031+ConorF123@users.noreply.github.com>
2023-04-23 21:47:34 -0600

    Completed WA13

--- js/WA13.js
+++ js/WA13.js
@@ -1,70 +1,21 @@
-function check() {
-    console.log('test');
-}
+var currentVolume = 50; // Set initial volume to 50
 
-function submit() {
-    alert(output.textContent);
-}
-
-function reset() {
-    outputInt = 0;
-    output.textContent = outputInt;
-}
-
-function minus() {
-    if (outputInt > 0) {
-    outputInt -=1;
-    output.textContent = outputInt; }
-    
-}
-
-function plus() {
-    if (outputInt < 100) {
-    outputInt +=1;
-    output.textContent = outputInt;
+function increaseVolume() {
+    var change = Math.floor(Math.random() * 15) + 1; // Generate random change
+    if (currentVolume + change > 100) {
+        currentVolume = 100; // Restrict volume to maximum of 100
+    } else {
+        currentVolume += change; // Increase volume by random change
     }
+    document.getElementById("volume").innerHTML = currentVolume; // Update display
 }
-
-function random() {
-    outputInt = randomNumber(0, 100);
-    output.textContent = outputInt;
-}
-
-function randomNumber(min, max) {
-    const num = Math.floor(Math.random() * (max - min + 1)) + min;
-    return num;
-  }
-
-
-
-const output = document.querySelector('.output');
-let outputInt = parseInt(output.textContent);
-console.log(outputInt);
-
-const minusButton = document.querySelector('.minus-button').addEventListener('click', minus);
-const plusButton = document.querySelector('.plus-button').addEventListener('click', plus);
-const resetButton = document.querySelector('.reset-button').addEventListener('click', reset);
-const randomButton = document.querySelector('.random-button').addEventListener('click', random);
-const submitButton = document.querySelector('.submit-button').addEventListener('click', submit);
-
-
-/* const button = document.querySelector('.button');
-const output = document.querySelector('.output');
-let phone_content = document.querySelector('.phone');
-button.addEventListener('click', updateOutput);
-function updateOutput() {
-    output.textContent = phone_content.value;
-    alert(phone_content.value);
-}
-*/
-
-
-var slider = document.getElementById("myRange");
-var sliderSubmit = document.querySelector(".slider-submit-button").addEventListener('click', update);
-var sliderOutput = document.querySelector(".slider-output");
-
-
-// Update the current slider value (each time you drag the slider handle)
-function update() {
-  sliderOutput.textContent = slider.value;
+      
+function decreaseVolume() {
+    var change = Math.floor(Math.random() * 15) + 1; // Generate random change
+    if (currentVolume - change < 0) {
+        currentVolume = 0; // Restrict volume to minimum of 0
+    } else {
+        currentVolume -= change; // Decrease volume by random change
+    }
+    document.getElementById("volume").innerHTML = currentVolume; // Update display
 }
